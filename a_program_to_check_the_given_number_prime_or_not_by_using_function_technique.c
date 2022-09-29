244c137c07232deff942b5cc1c62122d6ac57fa8
Prince <pkprincekumar2424110@gmail.com>
2022-09-29 19:56:40 +0530

    commitless

---
+++ a_program_to_check_the_given_number_prime_or_not_by_using_function_technique.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int prime(int n)
+{
+    int i,flag=0;
+    for(i=2;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            printf("given no is not prime");
+            flag=1;
+        }
+    }
+return(flag);
+}
+int main()
+{
+    int n;
+    printf("enter the number ");
+    scanf("%d",&n);
+    if(prime(n)==0)
+    {
+     printf("given number is prime");
+    }
+    return 0;
+}
